353fae20ceef2406e6dc647bd71d8c2a094021eb
Kyle Galloway <ckgalloway@crimson.ua.edu>
2016-04-01 13:25:35 -0500

    update

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,8 @@
 # Install Script
 # by Kyle Galloway
 # setup.sh
+# 
+# Need to run setup-repos first
 
 sudo add-apt-repository ppa:linrunner/tlp
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
@@ -8,20 +10,7 @@ sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y \
-    thermald \
-    git vim cmus \
-    zsh -y \
-    flashplugin-installer \
-    gcc g++ clang \
-    python-pip python-dev build-essential \
-    curl \
-    silversearcher-ag \
-    htop \
-    tudu \
-    terminator \
-    qalc \
-    cloc \
-    sublime-text-installer \
-    /
+LIST_OF_APPS="thermald vim cmus zsh flashplugin-installer gcc g++ clang python-pip python-dev build-essential curl silversearcher-ag htop tudu terminator qalc cloc sublime-text-installer"
+
+sudo apt-get install -y $LIST_OF_APPS
 
