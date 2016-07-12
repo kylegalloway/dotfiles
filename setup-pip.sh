88c31bfb9afab9200d1513abebdadbb283751057
Kyle Galloway <christopher.k.galloway@lmco.com>
2016-07-12 15:59:18 -0500

    Cleans branch

--- setup-pip.sh
+++
@@ -1,8 +0,0 @@
-# Install Script
-# by Kyle Galloway
-# setup-pip.sh
-
-echo "PIP"
-sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv
-sudo pip install Flask pycrypto livestreamer term2048 jrnl
