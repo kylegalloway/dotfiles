af376dfc285742811cd092878165569c8cd59b56
Kyle Galloway <kyle.galloway.1993@gmail.com>
2019-06-17 13:20:58 -0500

    feat: Added conky; cleaned up setup.sh

--- setup.sh
+++ setup.sh
@@ -22,19 +22,33 @@ change_shell() {
     chsh -s "$(which "${1}")"
 }
 
+zsh_setup(){
+    echo "Installing prezto"
+    install_pkg git
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+}
+
 zsh_use() {
     install_pkg zsh
     change_shell zsh
+    zsh_setup
 }
 
-oh_my_zsh_setup(){
-    echo "installing oh-my-zsh"
-    if which wget &>/dev/null; then
-        wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-    else
-        curl -L http://install.ohmyz.sh | sh
-    fi
-    git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME"/.oh-my-zsh
+conky_setup() {
+    echo "Installing conky"
+   conky_dir=${HOME}/.config/conky
+   mkdir -p ${conky_dir}
+   symlink "$DOTFILES"/conky/conky.conf ${conky_dir}
+}
+
+conky_use() {
+   install_pkg conky
+   install_pkg fonts-font-awesome
+   conky_setup
 }
 
 symlink() {
@@ -60,63 +74,12 @@ do_symlinking(){
     done
 }
 
-setup_repos(){
-    mkdir -p "$HOME"/Repos
-    git clone https://github.com/kylegalloway/dotfiles.git "$HOME"/Repos/dotfiles
-    git clone  https://github.com/simonwhitaker/gibo "$HOME"/Repos/gibo
-}
-
-dotfile_setup(){
-    cd "$DOTFILES" || exit
-    for f in ./.* ; do
-        BASENAME=$(basename "$f")
-        if [ "$BASENAME" != ".." ] || [ "$BASENAME" != "." ] || [ "$BASENAME" != ".git" ] || [ "$BASENAME" != ".gitignore" ]; then 
-		cp "$f" "${HOME}/$(basename "$f")"
-	fi
-    done
-}
-
 basic_setup() {
     echo -e "\033[1;30m- running basic setup..."
 #     sudo apt-get update &>/dev/null
 #     install_pkg git
-#     install_pkg tlp ppa:linrunner/tlp
-#     install_pkg xorg
-#     install_pkg network-manager
-#     install_pkg wireless-tools
-#     install_pkg wpasupplicant
-#     install_pkg alsa-base
-#     install_pkg volumeicon-alsa
-#     install_pkg suckless-tools
-#     install_pkg hal
-#     install_pkg acpi
-#     install_pkg spectrwm
-#     install_pkg conky-cli
-#     install_pkg thermald
-#     install_pkg vim
-#     install_pkg rxvt-unicode-256color
-#     install_pkg surf
-#     install_pkg gcc
-#     install_pkg g++
-#     install_pkg clang
-#     install_pkg curl
-#     install_pkg cmus
-#     install_pkg htop
-#     install_pkg silversearcher-ag
-#     install_pkg tudu
-#     install_pkg cloc
-#     install_pkg tty-clock
-#     install_pkg feh
-#     install_pkg rtorrent
-#     install_pkg xscreensaver
-#     install_pkg python-pip
-#     install_pkg python-dev
-#     install_pkg build-essential
 #     zsh_use
-#     oh_my_zsh_setup
-#     setup_repos
      do_symlinking
-#     dotfile_setup
     echo -e "\033[1;30m- Done!!..."
 }
 
