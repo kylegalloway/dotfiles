1bf7b384cfcaebd25f15217dc366617a38999952
Kyle Galloway <kyle.galloway.1993@gmail.com>
2019-08-28 17:04:31 -0500

    Update emacs init.el

--- files/init.el
+++ files/init.el
@@ -9,6 +9,8 @@
 (scroll-bar-mode -1)
 ; Add line numbers globally
 (global-linum-mode 1)
+; Remove line numbers in the terminal
+(add-hook 'term-mode-hook (lambda () (linum-mode -1)))
 
 (setq delete-old-versions -1 )		; delete excess backup versions silently
 (setq version-control t )		; use version control
@@ -35,10 +37,24 @@
 ;; Enable Org mode
 (require 'org)
 ;; Make Org mode work with files ending in .org
-;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; "Activate" org mode by making keybindings available
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-default-notes-file (concat org-directory "~/Repos/todo/notes.org"))
+(setq org-agenda-files (list "~/Repos/todo/notes.org"))
+(setq-default org-display-custom-times t)
+(setq org-time-stamp-custom-formats '("<%F>" . "<%b %e %Y %H:%M>"))
 
 ; Global auto-indent
 (define-key global-map (kbd "RET") 'newline-and-indent)
+
+; Shortcut to get to init.el
+(global-set-key (kbd "<f5>") (lambda() (interactive)(find-file "~/.emacs.d/init.el")))
+
+; Shortcut to get to todo.org
+(global-set-key (kbd "<f6>") (lambda() (interactive)(find-file "~/Repos/todo/todo.org")))
 
 ; Cua mode (normal copy/paste/cut/undo keys
 ;; C-x for cut
@@ -46,6 +62,18 @@
 ;; C-v for paste
 ;; C-z for undo
 (cua-mode 1)
+
+;; Add windmove keybindings for meta key
+(windmove-default-keybindings 'meta)
+
+;; Remove line nubmers in terminal
+(add-hook 'term-mode-hook 'my-inhibit-global-linum-mode)
+
+(defun my-inhibit-global-linum-mode ()
+  "Counter-act `global-linum-mode'."
+  (add-hook 'after-change-major-mode-hook
+            (lambda () (linum-mode 0))
+            :append :local))
 
 ;;;;;;; Install use-package
 ;; Update package-archive lists
@@ -82,12 +110,20 @@
   (setq evil-want-Y-yank-to-eol t)
   :config ;; tweak evil after loading it
   (evil-mode)
-  
+
+  ; Change initial states for certain modes
+  (evil-set-initial-state 'inferior-emacs-lisp-mode 'emacs)
+  (evil-set-initial-state 'shell-mode 'emacs)
+  (evil-set-initial-state 'term-mode 'emacs)
+  (evil-set-initial-state 'help-mode 'emacs)
+  (evil-set-initial-state 'dired-mode 'emacs)
+  (evil-set-initial-state 'org-mode 'emacs)
   ;; example how to map a command in normal mode (called 'normal state' in evil)
   ;(define-key evil-normal-state-map (kbd ", w") 'evil-window-vsplit))
   ;; example how to map a command in insert mode (called 'insert state' in evil)
   ;; (define-key evil-insert-state-map (kbd "jj") 'evil-normal-state)
 )
+  
 
 ; Install Evil Commentary (like Vim Commentary)
 (use-package evil-commentary
@@ -107,14 +143,43 @@
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 )
 
+(use-package multi-term
+  :ensure t
+  :init
+  (setq multi-term-program "/bin/zsh")
+  (setq multi-term-dedicated-select t)
+  ;; :config
+  ;; (global-set-key (kbd "<f2>") 'multi-term-dedicated-toggle)
+)
+
+; Open multi-term in project root
+(global-set-key (kbd "<f2>") 'projectile-multi-term-in-root)
+(defun projectile-multi-term-in-root ()
+  "Invoke `multi-term' in the project's root."
+  (interactive)
+  (projectile-with-default-dir (projectile-project-root) (multi-term-dedicated-toggle))
+  (multi-term-dedicated-select)
+)
+
+; Fix terminal colors inside emacs
+(use-package eterm-256color
+  :ensure t
+  :config
+  (add-hook 'term-mode-hook #'eterm-256color-mode)
+)
+
+; Try to make evil mode treat _ as part of words
+(add-hook 'after-change-major-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Repos/todo/todo.org")))
  '(package-selected-packages
    (quote
-    (projectile evil-commentary evil spacemacs-theme use-package))))
+    (eterm-256color multi-term projectile evil-commentary evil spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
