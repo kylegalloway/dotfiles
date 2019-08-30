ac919679ea66540ddf58465ab9bd21d19a38c544
Kyle Galloway <kyle.galloway.1993@gmail.com>
2019-08-29 21:38:27 -0500

    update

--- files/init.el
+++ files/init.el
@@ -13,6 +13,9 @@
 (add-hook 'term-mode-hook (lambda () (linum-mode -1)))
 ;; We don't want to type yes and no all the time so, do y and n
 (defalias 'yes-or-no-p 'y-or-n-p)
+;; I don't want any backups
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
 
 (setq delete-old-versions -1 )		; delete excess backup versions silently
 (setq version-control t )		; use version control
@@ -35,7 +38,9 @@
 ;; Enable transient mark mode
 (transient-mark-mode 1)
 
-;;;;Org mode configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org mode configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable Org mode
 (require 'org)
 ;; Make Org mode work with files ending in .org
@@ -48,6 +53,37 @@
 (setq org-agenda-files (list "~/Repos/todo/notes.org"))
 (setq-default org-display-custom-times t)
 (setq org-time-stamp-custom-formats '("<%F>" . "<%b %e %Y %H:%M>"))
+(setq org-log-done 'time)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "~/Repos/todo/todo.org" "New")
+         "* TODO [#A] %?\nCREATED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-mode agenda options                                                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; open agenda in current window
+(setq org-agenda-window-setup (quote current-window))
+;; warn me of any deadlines in next 7 days
+(setq org-deadline-warning-days 7)
+;; show me tasks scheduled or due in next fortnight
+(setq org-agenda-span (quote fortnight))
+;; don't show tasks as scheduled if they are already shown as a deadline
+(setq org-agenda-skip-scheduled-if-deadline-is-shown t)
+;; don't give awarning colour to tasks with impending deadlines
+;; if they are scheduled to be done
+(setq org-agenda-skip-deadline-prewarning-if-scheduled (quote pre-scheduled))
+;; don't show tasks that are scheduled or have deadlines in the
+;; normal todo list
+(setq org-agenda-todo-ignore-deadlines (quote all))
+(setq org-agenda-todo-ignore-scheduled (quote all))
+;; sort tasks in order of when they are due and then by priority
+(setq org-agenda-sorting-strategy
+  (quote
+   ((agenda deadline-up priority-down)
+    (todo priority-down category-keep)
+    (tags priority-down category-keep)
+    (search category-keep))))
+;; Removes "DONE" tasks from agenda view
+(setq org-agenda-skip-scheduled-if-done t)
 
 ;; Global auto-indent
 (define-key global-map (kbd "RET") 'newline-and-indent)
